9b6d10d04fa4b28d6f202e7284e07925077bd40f
AKaGaMI <akagami@AKaGaMIs-WorkSpace.local>
2025-07-08 11:43:39 +0800

    feat: add default image rekam medis

--- app/Models/RekamMedisPhoto.php
+++ app/Models/RekamMedisPhoto.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Helpers\ImageHelpers;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,12 @@ class RekamMedisPhoto extends Model
     use HasFactory;
 
     protected $guarded = [];
+    protected $appends = ['photos_url'];
+
+    public function getPhotosUrlAttribute()
+    {
+        return ImageHelpers::ImageCheckerHelpers($this->photos);
+    }
 
     public function hewan()
     {
